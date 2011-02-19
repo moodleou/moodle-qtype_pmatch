48c609ccc1b764d61d3b59d22ed86a44fef80c8e
jamiepratt <me@jamiep.org>
2011-02-19 19:29:01 +0700

    NOBUG implemented and successfully tested code to interpret most pmatch syntax fragments.

--- pmatchinterpreter.php
+++ pmatchinterpreter.php
@@ -1,6 +1,29 @@
 <?php
 abstract class qtype_pmatch_item{
-    abstract public function match($string, $start);
+    protected $matched;
+    public function interpret($string, $start){
+        list($found, $endofmatch) = $this->interpret_contents($string, $start);
+        if ($found){
+            $this->matched = substr($string, $start, $endofmatch-$start);
+        } else {
+            $this->matched = '';
+        }
+        return array($found, $endofmatch);
+    }
+    protected $pattern;
+    /**
+     * 
+     * Convert the $string starting at $start into a tree of object representing parts of pmatch code.
+     * This is the default method which is often overriden. It looks for $pattern which is a regex with no
+     * modifying options.
+     * @param string $string
+     * @param integer $start
+     */
+    protected function interpret_contents($string, $start){
+        //regex pattern to match one character of pmatch code
+        list($found, $endofpattern, $subpatterns) = $this->find_pattern($this->pattern, $string, $start);
+        return array($found, $endofpattern);
+    }
     /**
      * 
      * Invert the returned match for this item.
@@ -23,7 +46,6 @@ abstract class qtype_pmatch_item{
         } else {
             $endofpattern = $start;
         }
-        $not = $found;
         array_shift($matches);//pop off the matched string and only return sub patterns
         return array($found, $endofpattern, $matches);
     }
@@ -31,66 +53,208 @@ abstract class qtype_pmatch_item{
      * Is there a not here?
      * @param string $string the string to be match
      * @param the position to start at in the string
-     * @see qtype_pmatch_negatable_item::match()
+     * @see qtype_pmatch_negatable_item::interpret()
      */
-    public function match_not($string, $start){
+    public function interpret_not($string, $start){
         list($found, $endofpattern, $subpatterns) = $this->find_pattern('!^\s*not\s+!', $string, $start);
         return array($found, $endofpattern);
     }
 }
-class qtype_pmatch_match extends qtype_pmatch_item{
-    
-    protected function check_options($options){}
-    
-    public function match($string, $start){
+abstract class qtype_pmatch_item_with_contents extends qtype_pmatch_item{
+
+    protected $contents = array();
+
+    /**
+     * 
+     * Interpret sub contents of item.
+     * @param string $string code that is to be interpreted
+     * @param integer $start position at which to start
+     * @param array $branchfoundsofar (optional) items found so far, if any
+     * @return array (longest possible branch that matches the longest string,
+     *                string position after code for these items.)
+     */
+    protected function interpret_subcontents($string, $start, $branchfoundsofar = array()){
+        $typestotry = $this->next_possible_subcontent($branchfoundsofar);
+        $branchindex = 0;
+        $childbranches = array();
+        $childbranchcursor = array();
+        $endofchildbranch = array();
+        //iterate down all branches
+        foreach ($typestotry as $typetotry){
+            $childbranches[$branchindex] = $branchfoundsofar;
+            list($typefound, $found, $childbranchcursor[$branchindex]) = 
+                    $this->interpret_subcontent_item($typetotry, $string, $start);
+            if ($found){
+                $childbranches[$branchindex][] = $typefound;
+                list($childbranches[$branchindex], $childbranchcursor[$branchindex]) = 
+                    $this->interpret_subcontents($string, $childbranchcursor[$branchindex], $childbranches[$branchindex]);
+            }
+            $branchindex++;
+        }
+        //find the branch that matches the longest string
+        array_multisort($childbranchcursor, SORT_DESC, SORT_NUMERIC, $childbranches);
+        //print_object(compact('childbranchcursor', 'childbranches'));
+        return array(array_shift($childbranches), array_shift($childbranchcursor));
+    }
+    /**
+     * 
+     * What was the last type of sub contents found in $foundsofar
+     * @param array $foundsofar
+     * @return string the type of sub contents last found (prefix with 'qtype_pmatch_' to get classname)
+     */
+    protected function last_subcontent_type_found($foundsofar){
+        if (!empty($foundsofar)){
+            return substr(get_class($foundsofar[count($foundsofar)-1]), 13);
+        } else {
+            return '';
+        }
+    }
+    /**
+     * 
+     * In the branch of code matched so far what could be the next type.
+     * @param array $foundsofar
+     * @return array the types of sub contents that could come next
+     *                (prefix with 'qtype_pmatch_' to get classname)
+     */
+    protected function next_possible_subcontent($foundsofar){
+        return array();
+    }
+    /**
+     * 
+     * Try to match $cancontaintype in $string starting at $start.
+     * @param string $cancontaintype
+     * @param string $string
+     * @param integer $start 
+     */
+    protected function interpret_subcontent_item($cancontaintype, $string, $start){
+        $cancontainclassname = 'qtype_pmatch_'.$cancontaintype;
+        $cancontain = new $cancontainclassname();
+        list($found, $aftercontent) = $cancontain->interpret($string, $start);
+        if ($found) {
+            $founditem = $cancontain;
+            return array($founditem, true, $aftercontent);
+        } else {
+            return array(null, false, $start);
+        }
+    }
+    protected function interpret_contents($string, $start){
+        list($this->contents, $endofcontents) = $this->interpret_subcontents($string, $start);
+        return array((!empty($this->contents)), $endofcontents);
+    }
+}
+
+
+abstract class qtype_pmatch_match extends qtype_pmatch_item_with_contents{
+
+    abstract protected function interpret_options($options);
+
+    protected function interpret_contents($string, $start){
         //first check for a not
-        list(, $matchstart) = $this->match_not($string, $start);
+        list(, $matchstart) = $this->interpret_not($string, $start);
         $subpatterns = array();
-        list($found, $endofpattern, $subpatterns) = $this->find_pattern('!^pmatch(.*)\s*\(!', $string, $matchstart);
+        list($found, $endofopening, $subpatterns) = $this->find_pattern('!^match(.*)\s*\(!', $string, $matchstart);
+        if (!$found){
+            echo 'opening not matched';
+            return array(false, $start);
+        }
         if (!empty($subpatterns)){
             $options = $subpatterns[0][0];
         } else {
             $options = '';
         }
-        if (!$this->check_options($options)) {
-            $found = false;
+        if (!$this->interpret_options($options)) {
+            echo 'options not matched';
+            return array(false, $start);
         }
-        return array($found, $endofpattern);
+        list($this->contents, $endofcontents) = $this->interpret_subcontents($string, $endofopening);
+        if (empty($this->contents)){
+            echo 'contents not matched';
+            return array(false, $start);
+        }
+        list($found, $endofclosing, $subpatterns) = $this->find_pattern('!^\)\s*!', $string, $endofcontents);
+        if (!$found){
+            echo 'closing not matched';
+            echo substr($string, $endofcontents);
+            print_object($this);
+            return array(false, $start);
+        }
+        return array(true, $endofclosing);
     }
-    
-}
 
+}
+/*
 class qtype_pmatch_match_any extends qtype_pmatch_match{
-    protected function check_options($options){
+    protected function interpret_options($options){
         return ($options == '_any');
     }
 }
 
 class qtype_pmatch_match_all extends qtype_pmatch_match{
-    protected function check_options($options){
+    protected function interpret_options($options){
         return ($options == '_all');
     }
-}
+}*/
 
 class qtype_pmatch_match_options extends qtype_pmatch_match{
-    protected function check_options($options){
+    protected function interpret_options($options){
         return true;
+    }
+    protected function next_possible_subcontent($foundsofar){
+       return array('or_list', 'phrase');
+    }
+}
+class qtype_pmatch_or_list extends qtype_pmatch_item_with_contents{
+    protected function next_possible_subcontent($foundsofar){
+        switch ($this->last_subcontent_type_found($foundsofar)){
+            case '':
+            case 'or_character':
+                return array('or_list_phrase', 'word');
+            case 'word':
+            case 'or_list_phrase':
+                return array('or_character');
+        }
+    }
+}
+class qtype_pmatch_or_character extends qtype_pmatch_item{
+    protected $pattern = '!^\|!';
+}
+class qtype_pmatch_or_list_phrase extends qtype_pmatch_item_with_contents{
+    protected function interpret_contents($string, $start){
+        if ($string[$start] == '[') {
+            list($phrase, $found, $endofcontents) = $this->interpret_subcontent_item('phrase', $string, $start+1);
+            if ($found && $string[$endofcontents] == ']') {
+                $this->content = array($phrase);
+                return array(true, $endofcontents+1);
+            } else {
+                return array(false, $start); // TODO maybe bubble up an error here
+            }
+        }
+        return array(false, $start);
     }
 }
 
-class qtype_pmatch_word extends qtype_pmatch_item{
-    public function match($string, $start){
-        list($found, $endofpattern, $subpatterns) = $this->find_pattern('!^[a-z0-9]|(\\\\[\\\\ |?*])!', $string, $matchstart);
+class qtype_pmatch_phrase extends qtype_pmatch_item_with_contents{
+    protected function next_possible_subcontent($foundsofar){
+        switch ($this->last_subcontent_type_found($foundsofar)){
+            case '':
+            case 'word_delimiter':
+                return array('word');
+            case 'word':
+                return array('word_delimiter');
+        }
     }
-    private function can_contain(){
-        return array(new qtype_pmatch_special_character(), new qtype_pmatch_special_character());
+}
+class qtype_pmatch_word_delimiter extends qtype_pmatch_item{
+    protected $pattern = '!^[_\s]!';
+}
+class qtype_pmatch_word extends qtype_pmatch_item_with_contents{
+    protected function next_possible_subcontent($foundsofar){
+        return array('character_in_word', 'wildcard_in_word');
     }
 }
 class qtype_pmatch_character_in_word extends qtype_pmatch_item{
-    public function match($string, $start){
-        //regex pattern to match one character of pmatch code
-        list($found, $endofpattern, $subpatterns) = $this->find_pattern('!^[a-z0-9]|(\\\\[()\\\\ |?*_\[\]])!', $string, $start);
-        return array($found, $endofpattern);
-    }
+    protected $pattern = '!^[a-z0-9\!"#£$%&\'/\-+<=>@\^`{}~]|(\\\\[()\\\\ |?*_\[\]])!';
 }
-
+class qtype_pmatch_wildcard_in_word extends qtype_pmatch_item{
+    protected $pattern = '!^[?*]!';
+}
--- simpletest/testpmatchinterpreter.php
+++ simpletest/testpmatchinterpreter.php
@@ -30,29 +30,143 @@ require_once($CFG->dirroot . '/question/type/pmatch/pmatchinterpreter.php');
 
 
 class qtype_pmatch_interpreter extends UnitTestCase {
-    public function test_qtype_pmatch_all() {
-        $matchall = new qtype_pmatch_match_all();
-        $this->assertEqual(array(true, 5), $matchall->match_not(' not pmatch_all()', 0));
-        $this->assertEqual(array(true, 6), $matchall->match_not(' not  pmatch_all()', 0));
-        $this->assertEqual(array(false, 0), $matchall->match_not(' notpmatch_all()', 0));
-        $this->assertEqual(array(false, 2), $matchall->match_not(' notpmatch_all()', 2));
-        $this->assertEqual(array(true, 16), $matchall->match(' not pmatch_all()', 0));
-        $this->assertEqual(array(true, 17), $matchall->match(' not  pmatch_all()', 0));
-        $this->assertEqual(array(false, 0), $matchall->match(' notpmatch_all()', 0));
-        $this->assertEqual(array(false, 2), $matchall->match(' notpmatch_all()', 2));
+/*    public function test_qtype_pmatch_all() {
+        $interpretall = new qtype_pmatch_match_all();
+        $this->assertEqual(array(true, 5), $interpretall->interpret_not(' not pmatch_all()', 0));
+        $this->assertEqual(array(true, 6), $interpretall->interpret_not(' not  pmatch_all()', 0));
+        $this->assertEqual(array(false, 0), $interpretall->interpret_not(' notpmatch_all()', 0));
+        $this->assertEqual(array(false, 2), $interpretall->interpret_not(' notpmatch_all()', 2));
+        $this->assertEqual(array(true, 16), $interpretall->interpret(' not pmatch_all()', 0));
+        $this->assertEqual(array(true, 17), $interpretall->interpret(' not  pmatch_all()', 0));
+        $this->assertEqual(array(false, 0), $interpretall->interpret(' notpmatch_all()', 0));
+        $this->assertEqual(array(false, 2), $interpretall->interpret(' notpmatch_all()', 2));
+    }*/
+/*    public function test_qtype_pmatch_character_in_word() {
+        $interpretchar = new qtype_pmatch_character_in_word();
+        $this->assertEqual(array(true, 1), $interpretchar->interpret('f', 0));
+        $this->assertEqual(array(true, 2), $interpretchar->interpret('fF', 1));
+        //test interpreting of escaped special characters
+        $this->assertEqual(array(true, 2), $interpretchar->interpret('\_', 0));
+        $this->assertEqual(array(true, 2), $interpretchar->interpret('\]', 0));
+        $this->assertEqual(array(true, 2), $interpretchar->interpret('\[', 0));
+        $this->assertEqual(array(true, 2), $interpretchar->interpret('\_', 0));
+        $this->assertEqual(array(true, 2), $interpretchar->interpret('\(', 0));
+        $this->assertEqual(array(true, 2), $interpretchar->interpret('\)', 0));
+        $this->assertEqual(array(true, 2), $interpretchar->interpret('\ ', 0));
+        $this->assertEqual(array(true, 2), $interpretchar->interpret('\|', 0));
+        $this->assertEqual(array(true, 2), $interpretchar->interpret('\\\\', 0));
+        //should not interpret non escaped special characters
+        $this->assertEqual(array(false, 0), $interpretchar->interpret('|', 0));
+        $this->assertEqual(array(false, 0), $interpretchar->interpret(' ', 0));
+        $this->assertEqual(array(false, 0), $interpretchar->interpret('(', 0));
+        //should interpret escaped wild cards
+        $this->assertEqual(array(true, 2), $interpretchar->interpret('\*', 0));
+        $this->assertEqual(array(true, 2), $interpretchar->interpret('\?', 0));
+        //interpret other keyboard characters
+        $this->assertEqual(array(true, 1), $interpretchar->interpret('!', 0));
+        $this->assertEqual(array(true, 1), $interpretchar->interpret('"', 0));
+        $this->assertEqual(array(true, 1), $interpretchar->interpret('#', 0));
+        $this->assertEqual(array(true, 1), $interpretchar->interpret('£', 0));
+        $this->assertEqual(array(true, 1), $interpretchar->interpret('$', 0));
+        $this->assertEqual(array(true, 1), $interpretchar->interpret('%', 0));
+        $this->assertEqual(array(true, 1), $interpretchar->interpret('&', 0));
+        $this->assertEqual(array(true, 1), $interpretchar->interpret('\'', 0));
+        $this->assertEqual(array(true, 1), $interpretchar->interpret('/', 0));
+        $this->assertEqual(array(true, 1), $interpretchar->interpret('-', 0));
+        $this->assertEqual(array(true, 1), $interpretchar->interpret('+', 0));
+        $this->assertEqual(array(true, 1), $interpretchar->interpret('<', 0));
+        $this->assertEqual(array(true, 1), $interpretchar->interpret('=', 0));
+        $this->assertEqual(array(true, 1), $interpretchar->interpret('>', 0));
+        $this->assertEqual(array(true, 1), $interpretchar->interpret('@', 0));
+        $this->assertEqual(array(true, 1), $interpretchar->interpret('^', 0));
+        $this->assertEqual(array(true, 1), $interpretchar->interpret('`', 0));
+        $this->assertEqual(array(true, 1), $interpretchar->interpret('{', 0));
+        $this->assertEqual(array(true, 1), $interpretchar->interpret('}', 0));
+        $this->assertEqual(array(true, 1), $interpretchar->interpret('~', 0));
     }
-    public function test_qtype_pmatch_character_in_word() {
-        $matchchar = new qtype_pmatch_character_in_word();
-        $this->assertEqual(array(true, 1), $matchchar->match('f', 0));
-        $this->assertEqual(array(true, 2), $matchchar->match('fF', 1));
-        $this->assertEqual(array(true, 2), $matchchar->match('\_', 0));
-        $this->assertEqual(array(true, 2), $matchchar->match('\]', 0));
-        $this->assertEqual(array(true, 2), $matchchar->match('\[', 0));
-        $this->assertEqual(array(true, 2), $matchchar->match('\_', 0));
-        $this->assertEqual(array(true, 2), $matchchar->match('\(', 0));
-        $this->assertEqual(array(true, 2), $matchchar->match('\)', 0));
-        $this->assertEqual(array(true, 2), $matchchar->match('\ ', 0));
-        $this->assertEqual(array(true, 2), $matchchar->match('\|', 0));
-        $this->assertEqual(array(true, 2), $matchchar->match('\\\\', 0));
+    
+    public function test_qtype_pmatch_wildcard_in_word() {
+        $interpretchar = new qtype_pmatch_wildcard_in_word();
+
+        //should interpret wild cards
+        $this->assertEqual(array(true, 1), $interpretchar->interpret('*', 0));
+        $this->assertEqual(array(true, 1), $interpretchar->interpret('?', 0));
+
+    }
+    
+    public function test_qtype_pmatch_word() {
+        $interpretword = new qtype_pmatch_word();
+        $otherkeyboardchars = '!"#£$%&\'/-+<=>@^`{}~';
+        $this->assertEqual(array(true, strlen($otherkeyboardchars)), $interpretword->interpret($otherkeyboardchars, 0));
+        $alphanum = 'abcdefABCDEF1234567890';
+        $anotherword = 'fgdfgdfg';
+        $this->assertEqual(array(true, strlen($otherkeyboardchars)+strlen($alphanum)),
+                                $interpretword->interpret($otherkeyboardchars.$alphanum.' '.$anotherword, 0));
+        $this->assertEqual(array(true, strlen($otherkeyboardchars)+strlen($alphanum)+1+strlen($anotherword)),
+                                $interpretword->interpret($otherkeyboardchars.$alphanum.' '.$anotherword, strlen($otherkeyboardchars)+strlen($alphanum)+1));
+        $escapedspecialcharsword = '\\\\\\(\\)\\ \\|\\?\\*\\_\\[\\]'; // \() |?*_[]
+        $this->assertEqual(array(true, strlen($otherkeyboardchars)+strlen($alphanum)+1+strlen($anotherword)+1+strlen($escapedspecialcharsword)),
+                                $interpretword->interpret($otherkeyboardchars.$alphanum.' '.$anotherword.' '.$escapedspecialcharsword, strlen($otherkeyboardchars)+strlen($alphanum)+1+strlen($anotherword)+1));
+        $this->assertEqual(array(true, strlen($otherkeyboardchars)+strlen($alphanum)+2),
+                                $interpretword->interpret($otherkeyboardchars.$alphanum.'*? '.$anotherword, 0));
+        $embeddedwildcards = 'ab*cdefABC?DEF12*34567890';
+        $this->assertEqual(array(true, strlen($embeddedwildcards)),
+                                $interpretword->interpret($embeddedwildcards, 0));
+    }
+
+    public function test_qtype_pmatch_word_delimiter() {
+        $interpretworddelimiter = new qtype_pmatch_word_delimiter();
+
+        $this->assertEqual(array(true, 1), $interpretworddelimiter->interpret('_', 0));
+        $this->assertEqual(array(true, 3), $interpretworddelimiter->interpret('   ', 0));
+    }
+    public function test_qtype_pmatch_phrase() {
+        $interpretphrase = new qtype_pmatch_phrase();
+
+        $this->assertEqual(array(true, 16), $interpretphrase->interpret('hello_ginger top', 0));
+        $this->assertEqual(array(true, 6), $interpretphrase->interpret('googly', 0));
+        $this->assertEqual(array(false, 0), $interpretphrase->interpret('[]', 0));
+    }
+    public function test_qtype_pmatch_or_list_phrase() {
+        $interpretphrase = new qtype_pmatch_or_list_phrase();
+
+        $this->assertEqual(array(true, 18), $interpretphrase->interpret('[hello_ginger top]', 0));
+        $this->assertEqual(array(true, 8), $interpretphrase->interpret('[googly]', 0));
+        $this->assertEqual(array(false, 0), $interpretphrase->interpret('[]', 0));
+    }
+    public function test_qtype_pmatch_or_list() {
+        $interpretorlist = new qtype_pmatch_or_list();
+
+        $orlist = '[hello_ginger top]|googly|[googly]';
+        $this->assertEqual(array(true, strlen($orlist)), $interpretorlist->interpret($orlist, 0));
+        $this->assertEqual(array(true, 13), $interpretorlist->interpret('[googly]|popo', 0));
+        $this->assertEqual(array(false, 0), $interpretorlist->interpret('[]', 0));
+    }*/
+    public function test_qtype_pmatch_match_options() {
+
+        $interpretmatchoptions = new qtype_pmatch_match_options();
+        $matchwithoptions = 'match_mow(less*|smaller|low*|light*)';
+        $this->assertEqual(array(true, strlen($matchwithoptions)),
+                                $interpretmatchoptions->interpret($matchwithoptions, 0));
+
+        $interpretmatchoptions = new qtype_pmatch_match_options();
+        $matchwithoptions = 'match_mow(dens*|[specific gravity]|sg)';
+        $this->assertEqual(array(true, strlen($matchwithoptions)),
+                                $interpretmatchoptions->interpret($matchwithoptions, 0));
+
+        $interpretmatchoptions = new qtype_pmatch_match_options();
+        $matchwithoptions = 'match_mow(hello_ginger top)';
+        $this->assertEqual(array(true, strlen($matchwithoptions)),
+                                $interpretmatchoptions->interpret($matchwithoptions, 0));
+
+        $interpretmatchoptions = new qtype_pmatch_match_options();
+        $matchwithoptions = 'match_mow([specific gravity])';
+        $this->assertEqual(array(true, strlen($matchwithoptions)),
+                                $interpretmatchoptions->interpret($matchwithoptions, 0));
+        $interpretmatchoptions = new qtype_pmatch_match_options();
+        $matchwithoptions = 'match_mow(hello ginger top)';
+        $this->assertEqual(array(true, strlen($matchwithoptions)),
+                                $interpretmatchoptions->interpret($matchwithoptions, 0));
+
     }
 }
