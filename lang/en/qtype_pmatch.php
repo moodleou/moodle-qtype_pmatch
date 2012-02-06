fa4d80e3e9f24c7a86b3bfc86548f6de6814e18e
Tim Hunt <T.J.Hunt@open.ac.uk>
2012-02-06 08:04:58 +0100

    Update language strings to work with Moodle 2.2.

--- lang/en/qtype_pmatch.php
+++ lang/en/qtype_pmatch.php
@@ -25,25 +25,12 @@
  */
 
 
-$string['addingpmatch'] = 'Adding a Pattern match question';
-$string['anyotheranswer'] = 'Any other answer';
-$string['editingpmatch'] = 'Editing a Pattern match question';
-$string['pmatch'] = 'Pattern match';
-//intrepreter errors
-$string['ie_nomatchfound'] = 'Error in pattern match code.';
-$string['ie_unrecognisedsubcontents'] = 'Non recognized sub content in code fragment "{$a}".';
-$string['ie_missingclosingbracket'] = 'Missing closing bracket in code fragment "{$a}".';
-$string['ie_lastsubcontenttypeorcharacter'] = 'Or character must not end subcontent in "{$a}".';
-$string['ie_lastsubcontenttypeworddelimiter'] = 'Word delimiter character must not end sub content "{$a}".';
-$string['ie_illegaloptions'] = 'Illegal options in expression "match<strong><em>{$a}</em></strong>()".';
-$string['ie_unrecognisedexpression'] = 'Unrecognised expression.';
-
-
 $string['addmoreanswerblanks'] = 'Blanks for {no} More Answers';
 $string['addmoresynonymblanks'] = 'Blanks for {no} more synonyms';
 $string['allowsubscript'] = 'Allow use of subscript';
 $string['allowsuperscript'] = 'Allow use of superscript';
 $string['answeringoptions'] = 'Options For Entering Answers';
+$string['anyotheranswer'] = 'Any other answer';
 $string['applydictionarycheck'] = 'Check spelling of student';
 $string['answer'] = 'Answer: {$a}';
 $string['answermustbegiven'] = 'You must enter an answer if there is a grade or feedback.';
@@ -58,15 +45,24 @@ $string['filloutoneanswer'] = 'Use Pattern match syntax to describe correct answ
 $string['forcelength'] = 'If answer is more than 20 words';
 $string['forcelengthno'] = 'do not issue warning';
 $string['forcelengthyes'] = 'warn that answer is too long and invite respondee to shorten it';
-$string['forcelengthyes'] = 'warn that answer is too long and invite respondee to shorten it';
+$string['ie_nomatchfound'] = 'Error in pattern match code.';
+$string['ie_unrecognisedsubcontents'] = 'Non recognized sub content in code fragment "{$a}".';
+$string['ie_missingclosingbracket'] = 'Missing closing bracket in code fragment "{$a}".';
+$string['ie_lastsubcontenttypeorcharacter'] = 'Or character must not end subcontent in "{$a}".';
+$string['ie_lastsubcontenttypeworddelimiter'] = 'Word delimiter character must not end sub content "{$a}".';
+$string['ie_illegaloptions'] = 'Illegal options in expression "match<strong><em>{$a}</em></strong>()".';
+$string['ie_unrecognisedexpression'] = 'Unrecognised expression.';
 $string['inputareatoobig'] = 'The input area defined by "{$a}" is too big. Input area size is limited to a width of 150 characters and a height of 100 characters.';
 $string['nomatchingsynonymforword'] = 'No synonyms entered for word. Delete the word or enter synonym(s) for it.';
 $string['notenoughanswers'] = 'This type of question requires at least {$a} answers';
 $string['nomatchingwordforsynonym'] = 'You have not entered a word that the synonym is equivalent too. Delete the synonym(s) or enter an equivalent word for it.';
 $string['pleaseenterananswer'] = 'Please enter an answer.';
-$string['pmatch_help'] = 'In response to a question (that may include a image) the respondent types a short phrase. There may be several possible correct answers, each with a different grade. If the "Case sensitive" option is selected, then you can have different scores for "Word" or "word".';
-$string['pmatch_link'] = 'question/type/pmatch';
-$string['pmatchsummary'] = 'Allows a short response of one or a few sentences that is graded by comparing against various model answers, which are described using the OU\'s pattern match syntax.';
+$string['pluginname'] = 'Pattern match';
+$string['pluginname_help'] = 'In response to a question (that may include a image) the respondent types a short phrase. There may be several possible correct answers, each with a different grade. If the "Case sensitive" option is selected, then you can have different scores for "Word" or "word".';
+$string['pluginname_link'] = 'question/type/pmatch';
+$string['pluginnameadding'] = 'Adding a Pattern match question';
+$string['pluginnameediting'] = 'Editing a Pattern match question';
+$string['pluginnamesummary'] = 'Allows a short response of one or a few sentences that is graded by comparing against various model answers, which are described using the OU\'s pattern match syntax.';
 $string['repeatedword'] = 'This word appears more than once in synonym list.';
 $string['spellingmistakes'] = 'The following words are not in our dictionary : {$a}. Please correct your spelling.';
 $string['subsuponelineonly'] = 'The sub / super script editor can only be used with an input box one line high.';
