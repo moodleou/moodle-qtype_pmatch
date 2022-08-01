moodleou/moodle-qtype_oumultiresponse

Questions: Default options and privacy declaration for OU qtypes #467835

classes/privacy/provider.php
@@ -21,21 +21,77 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 namespace qtype_oumultiresponse\privacy;
+
+use \core_privacy\local\metadata\collection;
+use \core_privacy\local\request\transform;
+use \core_privacy\local\request\user_preference_provider;
+use \core_privacy\local\request\writer;
+
 defined('MOODLE_INTERNAL') || die();
 /**
- * Privacy Subsystem for qtype_oumultiresponse implementing null_provider.
+ * Privacy Subsystem for qtype_oumultiresponse implementing user_preference_provider.
  *
  * @copyright  2018 The Open University
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class provider implements \core_privacy\local\metadata\null_provider {
+class provider implements
+        // This component has data.
+        // We need to return default options that have been set a user preferences.
+        \core_privacy\local\metadata\provider,
+        \core_privacy\local\request\user_preference_provider {
+
     /**
-     * Get the language string identifier with the component's language
-     * file to explain why this plugin stores no data.
+     * Returns meta data about this system.
      *
-     * @return  string
+     * @param collection $collection The initialised collection to add items to.
+     * @return  collection     A listing of user data stored through this system.
      */
-    public static function get_reason() : string {
-        return 'privacy:metadata';
+    public static function get_metadata(collection $collection): collection {
+        $collection->add_user_preference('qtype_oumultiresponse_defaultmark', 'privacy:preference:defaultmark');
+        $collection->add_user_preference('qtype_oumultiresponse_penalty', 'privacy:preference:penalty');
+        $collection->add_user_preference('qtype_oumultiresponse_shuffleanswers', 'privacy:preference:shuffleanswers');
+        $collection->add_user_preference('qtype_oumultiresponse_answernumbering', 'privacy:preference:answernumbering');
+        $collection->add_user_preference('qtype_oumultiresponse_showstandardinstruction',
+                'privacy:preference:showstandardinstruction');
+        return $collection;
+    }
+
+    /**
+     * Export all user preferences for the plugin.
+     *
+     * @param int $userid The userid of the user whose data is to be exported.
+     */
+    public static function export_user_preferences(int $userid) {
+        $preference = get_user_preferences('qtype_oumultiresponse_defaultmark', null, $userid);
+        if (null !== $preference) {
+            $desc = get_string('privacy:preference:defaultmark', 'qtype_oumultiresponse');
+            writer::export_user_preference('qtype_oumultiresponse', 'defaultmark', $preference, $desc);
+        }
+
+        $preference = get_user_preferences('qtype_oumultiresponse_penalty', null, $userid);
+        if (null !== $preference) {
+            $desc = get_string('privacy:preference:penalty', 'qtype_oumultiresponse');
+            writer::export_user_preference('qtype_oumultiresponse', 'penalty', transform::percentage($preference), $desc);
+        }
+
+        $preference = get_user_preferences('qtype_oumultiresponse_answernumbering', null, $userid);
+        if (null !== $preference) {
+            $desc = get_string('privacy:preference:answernumbering', 'qtype_oumultiresponse');
+            writer::export_user_preference('qtype_oumultiresponse', 'answernumbering',
+                    get_string('answernumbering' . $preference, 'qtype_multichoice'), $desc);
+        }
+
+        $preference = get_user_preferences('qtype_oumultiresponse_shuffleanswers', null, $userid);
+        if (null !== $preference) {
+            $desc = get_string('privacy:preference:shuffleanswers', 'qtype_multichoice');
+            writer::export_user_preference('qtype_oumultiresponse', 'shuffleanswers', transform::yesno($preference), $desc);
+        }
+
+        $preference = get_user_preferences("qtype_oumultiresponse_showstandardinstruction", null, $userid);
+        if (null !== $preference) {
+            $desc = get_string("privacy:preference:showstandardinstruction", 'qtype_oumultiresponse');
+            writer::export_user_preference('qtype_oumultiresponse', 'showstandardinstruction',
+                    transform::yesno($preference), $desc);
+        }
     }
 }

questiontype.php
@@ -58,6 +58,13 @@ class qtype_oumultiresponse extends question_type {
         parent::get_question_options($question);
     }
 
+    public function save_defaults_for_new_questions(stdClass $fromform): void {
+        parent::save_defaults_for_new_questions($fromform);
+        $this->set_default_value('shuffleanswers', $fromform->shuffleanswers);
+        $this->set_default_value('answernumbering', $fromform->answernumbering);
+        $this->set_default_value('showstandardinstruction', $fromform->showstandardinstruction);
+    }
+
     public function save_question_options($question) {
         global $DB;
         $context = $question->context;

tests/privacy_provider_test.php
@@ -1,0 +1,108 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Privacy provider tests.
+ *
+ * @package    qtype_oumultiresponse
+ * @copyright  2021 The Open university
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+use core_privacy\local\metadata\collection;
+use \core_privacy\local\request\user_preference_provider;
+use qtype_oumultiresponse\privacy\provider;
+use core_privacy\local\request\writer;
+use core_privacy\local\request\transform;
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+require_once($CFG->dirroot . '/question/type/oumultiresponse/classes/privacy/provider.php');
+
+/**
+ * Privacy provider tests class.
+ *
+ * @package    qtype_oumultiresponse
+ * @copyright  2021 The Open university
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class qtype_oumultiresponse_privacy_provider_testcase extends \core_privacy\tests\provider_testcase {
+    // Include the privacy helper which has assertions on it.
+
+    public function test_get_metadata() {
+        $collection = new \core_privacy\local\metadata\collection('qtype_oumultiresponse');
+        $actual = \qtype_oumultiresponse\privacy\provider::get_metadata($collection);
+        $this->assertEquals($collection, $actual);
+    }
+
+    public function test_export_user_preferences_no_pref() {
+        $this->resetAfterTest();
+
+        $user = $this->getDataGenerator()->create_user();
+        provider::export_user_preferences($user->id);
+        $writer = writer::with_context(\context_system::instance());
+        $this->assertFalse($writer->has_any_data());
+    }
+
+    /**
+     * Test the export_user_preferences given different inputs
+     * @dataProvider user_preference_provider
+
+     * @param string $name The name of the user preference to get/set
+     * @param string $value The value stored in the database
+     * @param string $expected The expected transformed value
+     */
+    public function test_export_user_preferences($name, $value, $expected) {
+        $this->resetAfterTest();
+        $user = $this->getDataGenerator()->create_user();
+        set_user_preference("qtype_oumultiresponse_$name", $value, $user);
+        provider::export_user_preferences($user->id);
+        $writer = writer::with_context(\context_system::instance());
+        $this->assertTrue($writer->has_any_data());
+        $preferences = $writer->get_user_preferences('qtype_oumultiresponse');
+        foreach ($preferences as $key => $pref) {
+            $preference = get_user_preferences("qtype_oumultiresponse_{$key}", null, $user->id);
+            if ($preference === null) {
+                continue;
+            }
+            $desc = get_string("privacy:preference:{$key}", 'qtype_oumultiresponse');
+            $this->assertEquals($expected, $pref->value);
+            $this->assertEquals($desc, $pref->description);
+        }
+    }
+
+    /**
+     * Create an array of valid user preferences for the multiple response question type.
+     *
+     * @return array Array of valid user preferences.
+     */
+    public function user_preference_provider() {
+        return [
+                'default mark 2' => ['defaultmark', 2, 2],
+                'penalty 33.33333%' => ['penalty', 0.3333333, '33.33333%'],
+                'shuffle yes' => ['shuffleanswers', 1, 'Yes'],
+                'shuffle no' => ['shuffleanswers', 0, 'No'],
+                'answernumbering abc' => ['answernumbering', 'abc', 'a., b., c., ...'],
+                'answernumbering ABC' => ['answernumbering', 'ABCD', 'A., B., C., ...'],
+                'answernumbering 123' => ['answernumbering', '123', '1., 2., 3., ...'],
+                'answernumbering iii' => ['answernumbering', 'iii', 'i., ii., iii., ...'],
+                'answernumbering III' => ['answernumbering', 'IIII', 'I., II., III., ...'],
+                'show standard instruction yes' => ['showstandardinstruction', 1, 'Yes'],
+                'show standard instruction no' => ['showstandardinstruction', 0, 'No']
+        ];
+    }
+}

moodleou/moodle-qtype_pmatch

classes/privacy/provider.php
@@ -24,30 +24,116 @@
 
 namespace qtype_pmatch\privacy;
 
-defined('MOODLE_INTERNAL') || die();
-
 use core_privacy\local\metadata\collection;
 use core_privacy\local\request\writer;
 use core_privacy\local\request\transform;
 
+defined('MOODLE_INTERNAL') || die();
 /**
- * External dashboard provider for user preferences
+ * Privacy Subsystem for qtype_pmatch implementing user_preference_provider.
  *
- * @package qtype_pmatch
+ * @copyright  2018 The Open University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class provider implements \core_privacy\local\metadata\provider, \core_privacy\local\request\user_preference_provider {
+class provider implements
+        // This component has data.
+        // We need to return default options that have been set a user preferences.
+        \core_privacy\local\metadata\provider,
+        \core_privacy\local\request\user_preference_provider {
 
-    public static function get_metadata(collection $collection) : collection {
-        $collection->add_user_preference('qtype_pmatch_testquestion_pagesize', 'privacy:metadata:preference:pagesize');
+    /**
+     * Returns meta data about this system.
+     *
+     * @param collection $collection The initialised collection to add items to.
+     * @return  collection     A listing of user data stored through this system.
+     */
+    public static function get_metadata(collection $collection): collection {
+        $collection->add_user_preference('qtype_pmatch_testquestion_pagesize', 'privacy:preference:testquestion_pagesize');
+        $collection->add_user_preference('qtype_pmatch_defaultmark', 'privacy:preference:defaultmark');
+        $collection->add_user_preference('qtype_pmatch_penalty', 'privacy:preference:penalty');
+        $collection->add_user_preference('qtype_pmatch_usecase', 'privacy:preference:usecase');
+        $collection->add_user_preference('qtype_pmatch_allowsubscript', 'privacy:preference:allowsubscript');
+        $collection->add_user_preference('qtype_pmatch_allowsuperscript', 'privacy:preference:allowsuperscript');
+        $collection->add_user_preference('qtype_pmatch_applydictionarycheck', 'privacy:preference:applydictionarycheck');
+        $collection->add_user_preference('qtype_pmatch_sentencedividers', 'privacy:preference:sentencedividers');
+        $collection->add_user_preference('qtype_pmatch_converttospace', 'privacy:preference:converttospace');
         return $collection;
     }
 
+    /**
+     * Export all user preferences for the plugin.
+     *
+     * @param int $userid The userid of the user whose data is to be exported.
+     */
     public static function export_user_preferences(int $userid) {
         $pagesize = get_user_preferences('qtype_pmatch_testquestion_pagesize', null, $userid);
 
-        if (!is_null($pagesize)) {
-            writer::export_user_preference('qtype_pmatch', 'qtype_pmatch_testquestion_pagesize',
-                    $pagesize, get_string('privacy:metadata:preference:pagesize', 'qtype_pmatch'));
+        if (null !== ($pagesize)) {
+            $desc = get_string('privacy:preference:testquestion_pagesize', 'qtype_pmatch');
+            writer::export_user_preference('qtype_pmatch', 'qtype_pmatch_testquestion_pagesize', $pagesize, $desc);
+        }
+
+        $preference = get_user_preferences('qtype_pmatch_defaultmark', null, $userid);
+        if (null !== $preference) {
+            $desc = get_string('privacy:preference:defaultmark', 'qtype_pmatch');
+            writer::export_user_preference('qtype_pmatch', 'defaultmark', $preference, $desc);
+        }
+
+        $preference = get_user_preferences('qtype_pmatch_penalty', null, $userid);
+        if (null !== $preference) {
+            $desc = get_string('privacy:preference:penalty', 'qtype_pmatch');
+            writer::export_user_preference('qtype_pmatch', 'penalty', transform::percentage($preference), $desc);
+        }
+
+        $preference = get_user_preferences('qtype_pmatch_usecase', null, $userid);
+        if (null !== $preference) {
+            if ($preference) {
+                $stringvalue = get_string('caseyes', 'qtype_pmatch');
+            } else {
+                $stringvalue = get_string('caseno', 'qtype_pmatch');
+            }
+            $desc = get_string('privacy:preference:usecase', 'qtype_pmatch');
+            writer::export_user_preference('qtype_pmatch', 'usecase', $stringvalue, $desc);
+        }
+
+        $preferences = [
+                'allowsubscript',
+                'allowsuperscript'
+        ];
+        foreach ($preferences as $key) {
+            $preference = get_user_preferences("qtype_pmatch_{$key}", null, $userid);
+            if (null !== $preference) {
+                $desc = get_string("privacy:preference:{$key}", 'qtype_pmatch');
+                writer::export_user_preference('qtype_pmatch', $key, transform::yesno($preference), $desc);
+            }
+        }
+
+        $preference = get_user_preferences('qtype_pmatch_forcelength', null, $userid);
+        if (null !== $preference) {
+            if ($preference) {
+                $stringvalue = get_string('forcelengthyes', 'qtype_pmatch');
+            } else {
+                $stringvalue = get_string('forcelengthno', 'qtype_pmatch');
+            }
+            $desc = get_string('privacy:preference:forcelength', 'qtype_pmatch');
+            writer::export_user_preference('qtype_pmatch', 'forcelength', $stringvalue, $desc);
+        }
+
+        $preference = get_user_preferences('qtype_pmatch_applydictionarycheck', null, $userid);
+        if (null !== $preference) {
+            $desc = get_string('privacy:preference:applydictionarycheck', 'qtype_pmatch');
+            writer::export_user_preference('qtype_pmatch', 'applydictionarycheck', $preference, $desc);
+        }
+        $preference = get_user_preferences('qtype_pmatch_sentencedividers', null, $userid);
+        if (null !== $preference) {
+            $desc = get_string('privacy:preference:sentencedividers', 'qtype_pmatch');
+            writer::export_user_preference('qtype_pmatch', 'sentencedividers', $preference, $desc);
+        }
+
+        $preference = get_user_preferences('qtype_pmatch_converttospace', null, $userid);
+        if (null !== $preference) {
+            $desc = get_string('privacy:preference:converttospace', 'qtype_pmatch');
+            writer::export_user_preference('qtype_pmatch', 'converttospace', $preference, $desc);
         }
     }
 }

questiontype.php
@@ -77,6 +77,19 @@ class qtype_pmatch extends question_type {
         $this->delete_files_in_hints($questionid, $contextid);
     }
 
+    public function save_defaults_for_new_questions(stdClass $fromform): void {
+        parent::save_defaults_for_new_questions($fromform);
+        $this->set_default_value('usecase', $fromform->usecase);
+        $this->set_default_value('allowsubscript', $fromform->allowsubscript);
+        $this->set_default_value('allowsuperscript', $fromform->allowsuperscript);
+        $this->set_default_value('forcelength', $fromform->forcelength);
+        if (isset($fromform->applydictionarycheck)) {
+            $this->set_default_value('applydictionarycheck', $fromform->applydictionarycheck);
+        }
+        $this->set_default_value('sentencedividers', $fromform->sentencedividers);
+        $this->set_default_value('converttospace', $fromform->converttospace);
+    }
+
     public function save_question_options($questionform) {
         global $DB;
 

tests/privacy_provider_test.php
@@ -24,35 +24,90 @@
 
 namespace qtype_pmatch\tests;
 
+use core_privacy\local\metadata\collection;
+use \core_privacy\local\request\user_preference_provider;
+use qtype_pmatch\privacy\provider;
+use core_privacy\local\request\writer;
+use core_privacy\local\request\transform;
+
 defined('MOODLE_INTERNAL') || die();
 
-use core_privacy\local\request\writer;
+global $CFG;
+require_once($CFG->dirroot . '/question/type/pmatch/classes/privacy/provider.php');
 
-class privacy_provider_test extends \core_privacy\tests\provider_testcase {
+/**
+ * Privacy provider tests class.
+ *
+ * @package    qtype_pmatch
+ * @copyright  2021 The Open university
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class qtype_pmatch_privacy_provider_testcase extends \core_privacy\tests\provider_testcase {
+    // Include the privacy helper which has assertions on it.
 
-    public function test_export_user_preferences() {
+    public function test_get_metadata() {
+        $collection = new \core_privacy\local\metadata\collection('qtype_pmatch');
+        $actual = \qtype_pmatch\privacy\provider::get_metadata($collection);
+        $this->assertEquals($collection, $actual);
+    }
+
+    public function test_export_user_preferences_no_pref() {
         $this->resetAfterTest();
-        $generator = $this->getDataGenerator();
-        $user1 = $generator->create_user();
-        $user2 = $generator->create_user();
-        set_user_preferences([
-                'qtype_pmatch_testquestion_pagesize' => 5
-        ], $user1->id);
-        \qtype_pmatch\privacy\provider::export_user_preferences($user1->id);
-        $writer = writer::with_context(\context_system::instance());
-        $export = (object) [
-            'qtype_pmatch_testquestion_pagesize' => (object) [
-                'value' => 5,
-                'description' => get_string('privacy:metadata:preference:pagesize', 'qtype_pmatch')
-            ]
-        ];
-        $this->assertEquals($export, $writer->get_user_preferences('qtype_pmatch'));
 
-        // Test that another user with no user preferences doesn't get any data exported.
-        writer::reset();
-        \qtype_pmatch\privacy\provider::export_user_preferences($user2->id);
+        $user = $this->getDataGenerator()->create_user();
+        provider::export_user_preferences($user->id);
         $writer = writer::with_context(\context_system::instance());
-        $export = (object) [];
-        $this->assertEquals($export, $writer->get_user_preferences('qtype_pmatch'));
+        $this->assertFalse($writer->has_any_data());
+    }
+
+    /**
+     * Test the export_user_preferences given different inputs
+     * @dataProvider user_preference_provider
+
+     * @param string $name The name of the user preference to get/set
+     * @param string $value The value stored in the database
+     * @param string $expected The expected transformed value
+     */
+    public function test_export_user_preferences($name, $value, $expected) {
+        $this->resetAfterTest();
+        $user = $this->getDataGenerator()->create_user();
+        set_user_preference("qtype_pmatch_$name", $value, $user);
+        provider::export_user_preferences($user->id);
+        $writer = writer::with_context(\context_system::instance());
+        $this->assertTrue($writer->has_any_data());
+        $preferences = $writer->get_user_preferences('qtype_pmatch');
+        foreach ($preferences as $key => $pref) {
+            $preference = get_user_preferences("qtype_pmatch_{$key}", null, $user->id);
+            if ($preference === null) {
+                continue;
+            }
+            $desc = get_string("privacy:preference:{$key}", 'qtype_pmatch');
+            $this->assertEquals($expected, $pref->value);
+            $this->assertEquals($desc, $pref->description);
+        }
+    }
+
+    /**
+     * Create an array of valid user preferences for the pattern match question type.
+     *
+     * @return array Array of valid user preferences.
+     */
+    public function user_preference_provider() {
+        return [
+                'testquestion_pagesize2' => ['testquestion_pagesize', 5, 5],
+                'default mark 2' => ['defaultmark', 2, 2],
+                'penalty 33.33333%' => ['penalty', 0.3333333, '33.33333%'],
+                'use case yes' => ['usecase', 1, get_string('caseyes', 'qtype_pmatch')],
+                'use case no' => ['usecase', 0, get_string('caseno', 'qtype_pmatch')],
+                'allowsubscript yes' => ['allowsubscript', 1, 'Yes'],
+                'allowsubscript no' => ['allowsubscript', 0, 'No'],
+                'allowsuperscript yes' => ['allowsuperscript', 1, 'Yes'],
+                'allowsuperscript no' => ['allowsuperscript', 0, 'No'],
+                'forcelength yes' => ['forcelength', 1, get_string('forcelengthyes', 'qtype_pmatch')],
+                'forcelength no' => ['forcelength', 0, get_string('forcelengthno', 'qtype_pmatch')],
+                'applydictionarycheck yes' => ['applydictionarycheck', get_string('iso6391', 'langconfig'), 'en'],
+                'sentencedividers' => ['sentencedividers', '?.', '?.'],
+                'converttospace' => ['converttospace', ';:', ';:']
+        ];
     }
 }